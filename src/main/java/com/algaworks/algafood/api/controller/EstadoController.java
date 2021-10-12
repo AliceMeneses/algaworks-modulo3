57f0c9b249037ab37ab4426d8e8635e69729a907
AliceMeneses <alicesilvademeneses@gmail.com>
2021-10-12 15:47:13 -0300

    GET /cozinhas/{cozinhaId} e GET /estados/{estadosId}

--- src/main/java/com/algaworks/algafood/api/controller/EstadoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/EstadoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,4 +24,8 @@ public class EstadoController {
 		return estadoRepository.listar();
 	}
 	
+	@GetMapping("/{estadoId}")
+	public Estado buscar(@PathVariable Long estadoId) {
+		return estadoRepository.buscar(estadoId);
+	}
 }
